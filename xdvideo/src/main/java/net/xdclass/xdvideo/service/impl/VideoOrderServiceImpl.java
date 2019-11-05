566f3e593ed1e08a10505c1044f0175b03ab24f0
peter <610002997@qq.com>
2019-11-05 23:34:05 +0800

    微信支付之下单事务处理

--- xdvideo/src/main/java/net/xdclass/xdvideo/service/impl/VideoOrderServiceImpl.java
+++ xdvideo/src/main/java/net/xdclass/xdvideo/service/impl/VideoOrderServiceImpl.java
@@ -14,6 +14,8 @@ import net.xdclass.xdvideo.utils.HttpUtils;
 import net.xdclass.xdvideo.utils.WXPayUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Map;
@@ -37,6 +39,7 @@ public class VideoOrderServiceImpl implements VideoOrderService {
     private UserMapper userMapper;
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public String save(VideoOrderDto videoOrderDto) throws Exception {
         //查找视频信息
         Video video =  videoMapper.findById(videoOrderDto.getVideoId());
@@ -78,6 +81,7 @@ public class VideoOrderServiceImpl implements VideoOrderService {
 
     @Override
     public int updateVideoOderByOutTradeNo(VideoOrder videoOrder) {
+
         return videoOrderMapper.updateVideoOderByOutTradeNo(videoOrder);
     }
 
@@ -88,6 +92,7 @@ public class VideoOrderServiceImpl implements VideoOrderService {
      */
     private String unifiedOrder(VideoOrder videoOrder) throws Exception {
 
+        //int i = 1/0;   //模拟异常
         //生成签名
         SortedMap<String,String> params = new TreeMap<>();
         params.put("appid",weChatConfig.getAppId());
